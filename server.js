3d5af33314f694df874faac59fa2c5cdf30aaa33
Archishman <ag2324@it.jgec.ac.in>
2024-01-31 17:27:46 +0530

    Add swagger API documentation for all routes

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerJSDocs = YAML.load('./api-docs.yaml');
 
 connectDb();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
 app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler)
